68f541131ed2321278f18fa6c10755131a2b88c5
Kontham Trivikram Reddy <trivikramreddy868@gmail.com>
2026-04-04 11:28:26 +0530

    add express app setup with cors configuration

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "32kb" }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+export default app;
